ef21db6a123cc1c67ef51a62bfd128ffa6ac36f3
marceloexc <marceloexc@tutanota.de>
2025-04-26 01:18:02 -0500

    change detail view to inspector + fix bugs?

--- rm2000/Views/Sample Library/SampleLibraryView.swift
+++ rm2000/Views/Sample Library/SampleLibraryView.swift
@@ -12,6 +12,9 @@ struct SampleLibraryView: View {
 	
 	@State private var currentSamplesInView: Int = 0
 	@State private var selection = "Apple"
+	@State private var showInspector: Bool = true
+	
+	@State private var sliderValue = 50.0
 	
 	let options = ["Apple", "Banana"]
 	
@@ -23,10 +26,40 @@ struct SampleLibraryView: View {
 	var body: some View {
 		NavigationSplitView {
 			SidebarView(viewModel: viewModel)
-		} content: {
+				.toolbar(removing: .sidebarToggle)
+		} detail: {
 			DetailView(viewModel: viewModel)
-				.toolbar(id: "content-toolbar"){
-					
+				.toolbar(id: "rm2000.main-toolbar"){
+					if viewModel.finishedProcessing {
+						
+						ToolbarItem(id: "rm2000.open-in-finder-button", placement: .primaryAction) {
+							OpenInFinderButton()
+						}
+						ToolbarItem(id: "rm2000.import-sample-button", placement: .primaryAction) {
+							ImportSampleButton()
+						}
+						ToolbarItem(id: "rm2000.picker", placement: .primaryAction) {
+							Picker("View settings", selection: $selection) {
+								Label("Grid", systemImage: "square.grid.2x2")
+								Label("List", systemImage: "list.bullet")
+							}.pickerStyle(.inline)
+						}
+						ToolbarItem(id: UUID().uuidString, placement: .favoritesBar) {
+							Picker("View settings", selection: $selection) {
+								Label("Grid", systemImage: "play.fill")
+								Label("List", systemImage: "forward.fill")
+							}.pickerStyle(.inline)
+						}
+						ToolbarItem(id: "rm2000.sidebar", placement: .navigation) {
+							SidebarButton()
+						}
+						ToolbarItem(id: UUID().uuidString, placement: .favoritesBar) {
+							Slider(value: $sliderValue, in: 0...100)
+						}
+						ToolbarItem(id: "rm2000.spacer", placement: .favoritesBar) {
+							Spacer()
+						}
+					}
 					/*
 					 theres this gnarly bug where if i select "Icon and Text" in the
 					 context menu of the toolbar, the sidebar button (now with the
@@ -45,36 +78,33 @@ struct SampleLibraryView: View {
 					// UUID() as the id's to workaround a nasty swiftui bug
 					
 					// or else they just wont show up...stupid...
-					ToolbarItem(id: UUID().uuidString) {
-						Picker("View settings", selection: $selection) {
-							Label("Grid", systemImage: "square.grid.2x2")
-							Label("List", systemImage: "list.bullet")
-						}.pickerStyle(.inline)
-					}
-					ToolbarItem(id:UUID().uuidString){
-						ImportSampleButton()
-					}
-					ToolbarItem(id: UUID().uuidString, placement: .favoritesBar) {
-						Text("Player")
-					}
+
 				}
-		} detail: {
+		}
+		.inspector(isPresented: $showInspector) {
 			let testFile = URL(fileURLWithPath: "/Users/marceloexc/Developer/replica/rm2000Tests/Example--sample.aac")
 			let recording = TemporaryActiveRecording(fileURL: testFile)
 			EditSampleView(recording: recording) { _, _, _ in
 				// Empty completion handler
-				
 			}
-			.toolbar(id: "detail-toolbar") {
-				ToolbarItem(id: "spacer") {
+			.inspectorColumnWidth(min: 90, ideal: 300, max: 400)
+			.toolbar(id: "rm2000.inspector.toolbar") {
+				
+				ToolbarItem(id: "rm2000.spacer") {
 					Spacer()
 				}
-				ToolbarItem(id: "finder-button") {
-					OpenInFinderButton()
+				
+				ToolbarItem(id: "rm2000.inspector.button") {
+					Button {
+						showInspector.toggle()
+					} label: {
+						Label("Toggle Inspector", systemImage: "sidebar.right")
+					}
 				}
+
 			}
 		}
-		.toolbarRole(.automatic)
+		.toolbarRole(.editor)
 		.navigationTitle("Sample Library")
 		.navigationSubtitle("\(currentSamplesInView) Samples")
 
@@ -83,6 +113,20 @@ struct SampleLibraryView: View {
 		}
 		.searchable(text: .constant(""), placement: .sidebar)
 	}
+}
+
+struct SidebarButton: View {
+	var body: some View {
+		Button(action: toggleSidebar, label: {
+			Image(systemName: "sidebar.leading")
+		})
+	}
+}
+
+private func toggleSidebar() {
+#if os(macOS)
+	NSApp.keyWindow?.firstResponder?.tryToPerform(#selector(NSSplitViewController.toggleSidebar(_:)), with: nil)
+#endif
 }
 
 struct OpenInFinderButton: View {
@@ -152,4 +196,5 @@ class SampleLibraryViewModel: ObservableObject {
 #Preview {
 	SampleLibraryView()
 		.environmentObject(SampleStorage.shared)
+		.frame(width: 900)
 }

dcef6661e0c62489189e3e455c6205c83a98ea54
marceloexc <marceloexc@tutanota.de>
2025-05-15 16:49:06 -0500

    move AVPlayerView extension out of `Shared.swift`

--- rm2000/Views/Sample Editing/TrimmingPlayerView.swift
+++ rm2000/Views/Sample Editing/TrimmingPlayerView.swift
@@ -135,3 +135,58 @@ struct AudioPlayerView: NSViewRepresentable {
 	
 	func updateNSView(_ nsView: AVPlayerView, context: Context) {}
 }
+
+extension AVPlayerView {
+	/**
+	 Activates trim mode without waiting for trimming to finish.
+	 */
+	func activateTrimming() async throws { // TODO: `throws(CancellationError)`.
+		_ = await updates(for: \.canBeginTrimming).first { $0 }
+		
+		try Task.checkCancellation()
+		
+		Task {
+			await beginTrimming()
+		}
+		
+		await Task.yield()
+	}
+	
+	func hideTrimButtons() {
+		// This method is a collection of hacks, so it might be acting funky on different OS versions.
+		guard
+			let avTrimView = firstSubview(deep: true, where: { $0.simpleClassName == "AVTrimView" }),
+			let superview = avTrimView.superview
+		else {
+			return
+		}
+		
+		// First find the constraints for `avTrimView` that pins to the left edge of the button.
+		// Then replace the left edge of a button with the right edge - this will stretch the trim view.
+		if let constraint = superview.constraints.first(where: {
+			($0.firstItem as? NSView) == avTrimView && $0.firstAttribute == .right
+		}) {
+			superview.removeConstraint(constraint)
+			constraint.changing(secondAttribute: .right).isActive = true
+		}
+		
+		if let constraint = superview.constraints.first(where: {
+			($0.secondItem as? NSView) == avTrimView && $0.secondAttribute == .right
+		}) {
+			superview.removeConstraint(constraint)
+			constraint.changing(firstAttribute: .right).isActive = true
+		}
+		
+		// Now find buttons that are not images (images are playing controls) and hide them.
+		superview.subviews
+			.first { $0 != avTrimView }?
+			.subviews
+			.filter { ($0 as? NSButton)?.image == nil }
+			.forEach {
+				$0.isHidden = true
+			}
+	}
+	
+	open override func cancelOperation(_ sender: Any?) {}
+	
+}

755328850c0b29c445af55bcd82cc61040c21071
marceloexc <marceloexc@tutanota.de>
2025-06-07 14:01:22 -0500

    chore: run swift-format

--- rm2000/Views/Main Window/LCDScreenView.swift
+++ rm2000/Views/Main Window/LCDScreenView.swift
@@ -1,65 +1,66 @@
 import SwiftUI
 
 struct LCDScreenView: View {
-	@EnvironmentObject private var recordingState: TapeRecorderState
-	
-	var body: some View {
-		ZStack {
-			
-			if recordingState.status == .recording {
-				Image("LCDScreenFrameRecording")
-					.resizable()
-					.scaledToFit()
-					.frame(width: 300)
-					.offset(x: 0, y: 0)
-			} else {
-				Image("LCDScreenFrameInactive")
-					.resizable()
-					.scaledToFit()
-					.frame(width: 300)
-					.offset(x: 0, y: 0)
-			}
-			LCDSymbolGlyphs()
-			
-			Image("LCDOuterGlow")
-				.resizable()
-				.frame(width: 330)
-		}
-	}
+  @EnvironmentObject private var recordingState: TapeRecorderState
+
+  var body: some View {
+    ZStack {
+
+      if recordingState.status == .recording {
+        Image("LCDScreenFrameRecording")
+          .resizable()
+          .scaledToFit()
+          .frame(width: 300)
+          .offset(x: 0, y: 0)
+      } else {
+        Image("LCDScreenFrameInactive")
+          .resizable()
+          .scaledToFit()
+          .frame(width: 300)
+          .offset(x: 0, y: 0)
+      }
+      LCDSymbolGlyphs()
+
+      Image("LCDOuterGlow")
+        .resizable()
+        .frame(width: 330)
+    }
+  }
 }
 
 struct LCDSymbolGlyphs: View {
-	@EnvironmentObject private var recordingState: TapeRecorderState
+  @EnvironmentObject private var recordingState: TapeRecorderState
 
-	var body: some View {
-		HStack(alignment: .center) { // entire view
-      VStack(alignment: .leading) { // left side of the screen
-				HStack { // top glpyhs)
-					VStack(alignment: .leading, spacing: 4) {
-						LCDTextCaptionWithGradient("STEREO 44.1kHz")
-						
-						HStack(spacing: 6) {
-							
-							// todo - just make donutsspinner have an @EnvrionmentObject of recordingState
-							if recordingState.status == .recording {
-								DonutSpinner(direction: .counterclockwise, active: true)
-								DonutSpinner(direction: .clockwise, active: true)
-							} else {
-								DonutSpinner(direction: .counterclockwise, active: false)
-								DonutSpinner(direction: .clockwise, active: false)
-							}
+  var body: some View {
+    HStack(alignment: .center) {  // entire view
+      VStack(alignment: .leading) {  // left side of the screen
+        HStack {  // top glpyhs)
+          VStack(alignment: .leading, spacing: 4) {
+            LCDTextCaptionWithGradient("STEREO 48.0kHz")
 
-							RecordingGlyph()
-							SourceGlyph()
-							ErrorGlyph()
-						}
-					}.frame(width: 125, height: 40)
-						.padding(.trailing, -20)
-				}
-				
-				VStack(alignment: .leading) {
-					LCDTextBig(recordingState.sampleRecordAudioFormat.asString.uppercased())
-					
+            HStack(spacing: 6) {
+
+              // todo - just make donutsspinner have an @EnvrionmentObject of recordingState
+              if recordingState.status == .recording {
+                DonutSpinner(direction: .counterclockwise, active: true)
+                DonutSpinner(direction: .clockwise, active: true)
+              } else {
+                DonutSpinner(direction: .counterclockwise, active: false)
+                DonutSpinner(direction: .clockwise, active: false)
+              }
+
+              RecordingGlyph()
+              SourceGlyph()
+              ErrorGlyph()
+            }
+          }.frame(width: 125, height: 40)
+            .padding(.trailing, -20)
+        }
+
+        VStack(alignment: .leading) {
+          LCDTextBig(
+            recordingState.sampleRecordAudioFormat.asString.uppercased())
+
           VStack {
             if recordingState.status == .recording {
               LCDTextBig(timeString(recordingState.elapsedTimeRecording))
@@ -67,165 +68,150 @@ struct LCDSymbolGlyphs: View {
               LCDTextBig("STBY")
             }
           }
-          .mask(LinearGradient(
-            colors: [Color(hex: 0x220300, alpha: 0.16),
-                     Color(hex: 0x220300)],
-            startPoint: .bottom,
-            endPoint: .top
-          ))
-				}
+          .mask(
+            LinearGradient(
+              colors: [
+                Color(hex: 0x220300, alpha: 0.16),
+                Color(hex: 0x220300),
+              ],
+              startPoint: .bottom,
+              endPoint: .top
+            ))
+        }
         .frame(maxWidth: 150, alignment: .leading)
         .offset(x: -13)
 
-			}
-			
-			VUMeter()
-				.mask(LinearGradient(
-          colors: [Color(hex: 0x220300, alpha: 0),
-                   Color(hex: 0x220300, alpha: 0.45),
-									 Color(hex: 0x220300)],
-					startPoint: .bottom,
-					endPoint: .top
-				))
-				.colorEffect(Shader(function: .init(library: .default, name: "dotMatrix"), arguments: []))
-				.shadow(color: .black.opacity(0.35), radius: 1, x: 2, y: 4)
+      }
 
-				.frame(width: 60, height: 155)
-				.padding(.leading, -20)
+      VUMeter()
+        .mask(
+          LinearGradient(
+            colors: [
+              Color(hex: 0x220300, alpha: 0),
+              Color(hex: 0x220300, alpha: 0.45),
+              Color(hex: 0x220300),
+            ],
+            startPoint: .bottom,
+            endPoint: .top
+          )
+        )
+        .colorEffect(
+          Shader(
+            function: .init(library: .default, name: "dotMatrix"), arguments: []
+          )
+        )
+        .shadow(color: .black.opacity(0.35), radius: 1, x: 2, y: 4)
+
+        .frame(width: 60, height: 155)
+        .padding(.leading, -20)
         .offset(x: 3)
-			// todo - too close. claustrophobic
-		}	.frame(width: 200, height: 168)
-	}
+      // todo - too close. claustrophobic
+    }.frame(width: 200, height: 168)
+  }
 }
 
 struct LCDTextStyle: ViewModifier {
-	func body(content: Content) -> some View {
-		content
+  func body(content: Content) -> some View {
+    content
 
-	}
+  }
 }
 
 extension Font {
-	static let tachyoFont = Font.custom("Tachyo", size: 41)
-	static let tachyoFontBig = Font.custom("Tachyo", size: 61)
-	static let tasaFont = Font.custom("TASAExplorer-SemiBold", size: 14)
+  static let tachyoFont = Font.custom("Tachyo", size: 41)
+  static let tachyoFontBig = Font.custom("Tachyo", size: 61)
+  static let tasaFont = Font.custom("TASAExplorer-SemiBold", size: 14)
 }
 
 extension View {
-	func LCDText() -> some View {
-		modifier(LCDTextStyle())
-	}
+  func LCDText() -> some View {
+    modifier(LCDTextStyle())
+  }
 }
 
 struct LCDTextCaption: View {
-	var title: String
-	
-	init(_ title: String) {
-		self.title = title
-	}
-	
-	var body: some View {
-		Text(title)
-			.foregroundColor(Color("LCDTextColor"))
-			.shadow(color: .black.opacity(0.25), radius: 1, x: 0, y: 4)
-			.font(Font.tasaFont)
-	}
+  var title: String
+
+  init(_ title: String) {
+    self.title = title
+  }
+
+  var body: some View {
+    Text(title)
+      .foregroundColor(Color("LCDTextColor"))
+      .shadow(color: .black.opacity(0.25), radius: 1, x: 0, y: 4)
+      .font(Font.tasaFont)
+  }
 }
 
 struct LCDTextCaptionWithGradient: View {
-	var title: String
-	
-	init(_ title: String) {
-		self.title = title
-	}
-	
-	var body: some View {
-		Text(title)
-			.foregroundStyle(LinearGradient(
-				colors: [Color(hex: 0x220300, alpha: 0.32),
-								 Color(hex: 0x220300)],
-				startPoint: .top,
-				endPoint: .bottom
-			))
-			.shadow(color: .black.opacity(0.25), radius: 1, x: 0, y: 4)
-			.font(Font.tasaFont)
-	}
+  var title: String
+
+  init(_ title: String) {
+    self.title = title
+  }
+
+  var body: some View {
+    Text(title)
+      .foregroundStyle(
+        LinearGradient(
+          colors: [
+            Color(hex: 0x220300, alpha: 0.32),
+            Color(hex: 0x220300),
+          ],
+          startPoint: .top,
+          endPoint: .bottom
+        )
+      )
+      .shadow(color: .black.opacity(0.25), radius: 1, x: 0, y: 4)
+      .font(Font.tasaFont)
+  }
 }
 
 struct LCDTextBig: View {
-	var title: String
-	
-	init(_ title: String) {
-		self.title = title
-	}
-	
-	var body: some View {
-		Text(" \(title) ")
-			.foregroundColor(Color("LCDTextColor"))
-			.shadow(color: .black.opacity(0.25), radius: 1, x: 0, y: 4)
-			.font(Font.tachyoFont)
-			.fontWeight(.medium)
-			.fixedSize()
-			.kerning(-1.5)
-	}
-}
+  var title: String
 
-struct LCDTextBigWithGradient: View {
-	var title: String
-	
-	init(_ title: String) {
-		self.title = title
-	}
-	
-	var body: some View {
-		Text(" \(title) ")
-			.foregroundStyle(LinearGradient(
-				colors: [Color(hex: 0x220300, alpha: 0.32),
-								 Color(hex: 0x220300)],
-				startPoint: .bottom,
-				endPoint: .top
-			))
-			.shadow(color: .black.opacity(0.25), radius: 1, x: 0, y: 4)
-			.font(Font.tachyoFont)
-			.fontWeight(.medium)
-			.fixedSize()
-			.offset(x: -15)
-			.kerning(-1.5)
-	}
+  init(_ title: String) {
+    self.title = title
+  }
+
+  var body: some View {
+    Text(" \(title) ")
+      .foregroundColor(Color("LCDTextColor"))
+      .shadow(color: .black.opacity(0.25), radius: 1, x: 0, y: 4)
+      .font(Font.tachyoFont)
+      .fontWeight(.medium)
+      .fixedSize()
+      .kerning(-1.5)
+  }
 }
 
 struct LCDTextBigWithGradientHUD: View {
-	var title: String
-	
-	init(_ title: String) {
-		self.title = title
-	}
-	
-	var body: some View {
-		Text(" \(title) ")
-			.foregroundStyle(LinearGradient(
-				colors: [Color(hex: 0x220300, alpha: 0.32),
-								 Color(hex: 0x220300)],
-				startPoint: .bottom,
-				endPoint: .top
-			))
-			.shadow(color: .black.opacity(0.25), radius: 1, x: 0, y: 4)
-			.font(Font.tachyoFontBig)
-			.fontWeight(.medium)
-			.fixedSize()
-			.offset(x: -15)
-			.kerning(-1.5)
-	}
+  var title: String
+
+  init(_ title: String) {
+    self.title = title
+  }
+
+  var body: some View {
+    Text(" \(title) ")
+      .shadow(color: .black.opacity(0.25), radius: 1, x: 0, y: 4)
+      .font(Font.tachyoFontBig)
+      .fontWeight(.medium)
+      .fixedSize()
+      .offset(x: -15)
+      .kerning(-1.5)
+  }
 }
 
 #Preview("LCD Screen") {
-	LCDScreenView()
-		.environmentObject(TapeRecorderState())
+  LCDScreenView()
+    .environmentObject(TapeRecorderState())
 }
 
 #Preview("LCD Symbols") {
-	LCDSymbolGlyphs()
-		.environmentObject(TapeRecorderState())
-		.border(.black)
-		.padding()
+  LCDSymbolGlyphs()
+    .environmentObject(TapeRecorderState())
+    .border(.black)
+    .padding()
 }

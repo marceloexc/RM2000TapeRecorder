22cbcb96eb270e19a7ead3399a376658b8f25664
marceloexc <marceloexc@tutanota.de>
2025-06-09 21:44:04 -0500

    remove user notifications for now

--- rm2000/Views/UserNotifications.swift
+++ rm2000/Views/UserNotifications.swift
@@ -1,27 +1,28 @@
 import UserNotifications
 
 func displayTestingGlobalNotication() async {
-	let center = UNUserNotificationCenter.current()
+  // TODO implement this when I actually know what I'm doing
+//	let center = UNUserNotificationCenter.current()
+//	
+//	do {
+//		try await center.requestAuthorization(options: [.alert, .criticalAlert, .provisional])
+//	} catch {
+//		print("User Notifications not enabled.")
+//	}
+//	
+//	let content = UNMutableNotificationContent()
+//	
+//	content.title = "RM2000 Tape Recorder"
+//	content.body = "Sample Recorded!"
+//	let uuid = UUID().uuidString
+//	
+//	let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 0.1, repeats: false)
+//	print(trigger)
+//	let request = UNNotificationRequest(identifier: uuid, content: content, trigger: trigger)
 	
-	do {
-		try await center.requestAuthorization(options: [.alert, .criticalAlert, .provisional])
-	} catch {
-		print("User Notifications not enabled.")
-	}
-	
-	let content = UNMutableNotificationContent()
-	
-	content.title = "RM2000 Tape Recorder"
-	content.body = "Sample Recorded!"
-	let uuid = UUID().uuidString
-	
-	let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 0.1, repeats: false)
-	print(trigger)
-	let request = UNNotificationRequest(identifier: uuid, content: content, trigger: trigger)
-	
-	do {
-		try await center.add(request)
-	} catch {
-		print("not enabled")
-	}
+//	do {
+//		try await center.add(request)
+//	} catch {
+//		print("not enabled")
+//	}
 }

9d14425edc4033c840ea8d865a8343b81b596ea5
marceloexc <marceloexc@tutanota.de>
2025-07-24 14:36:29 -0500

    add Waveform to table and pause when resizing

--- rm2000/Views/Sample Library/Detail Views/TableView.swift
+++ rm2000/Views/Sample Library/Detail Views/TableView.swift
@@ -2,6 +2,13 @@ import SwiftUI
 
 struct RecordingsTableView: View {
   @ObservedObject var viewModel: SampleLibraryViewModel
+  
+  // the waveform package may crash the app when resizing because
+  // it doesn't know when to stop and just recomputes the waveform
+  // randomly.
+  
+  // this binding makes us manually stop that
+  @State private var isResizing: Bool = false
   let viewType: SampleFilterPredicate
   
   private var filteredSamples: [Sample] {
@@ -15,14 +22,23 @@ struct RecordingsTableView: View {
     }
   }
   
-  
     var body: some View {
       Table(filteredSamples) {
         TableColumn("Name", value: \.filename!)
         TableColumn("Title", value: \.title)
         TableColumn("Tags") { sample in
-          Text("\(sample.tags)")
+          Text(sample.tags.joined(separator: ", "))
         }
+        TableColumn("Waveform") { sample in
+          StaticWaveformView(fileURL: sample.fileURL, isPaused: $isResizing)
+            .frame(maxWidth: 200)
+        }
+      }
+      .onReceive(NotificationCenter.default.publisher(for: NSWindow.didResizeNotification)) { notification in
+        isResizing = true
+      }
+      .onReceive(NotificationCenter.default.publisher(for: NSWindow.didEndLiveResizeNotification)) { notification in
+        isResizing = false
       }
     }
 }
--- rm2000/Views/Sample Library/WaveformView.swift
+++ rm2000/Views/Sample Library/WaveformView.swift
@@ -12,22 +12,44 @@ import DSWaveformImageViews
 struct StaticWaveformView: View {
 	
 	var fileURL: URL
+  @Binding var isPaused: Bool
 	
 	var configuration: Waveform.Configuration = Waveform.Configuration(
 		style: .striped(Waveform.Style.StripeConfig(color: .white.withAlphaComponent(0.6), width: 2, spacing: 1, lineCap: .butt)),
 		verticalScalingFactor: 1.0
 	)
+  
+  private var randomID: String {
+    return UUID.init().uuidString
+  }
+  
+  init(fileURL: URL) {
+    self.fileURL = fileURL
+    self._isPaused = .constant(false)
+  }
+  
+  init(fileURL: URL, isPaused: Binding<Bool>) {
+    self.fileURL = fileURL
+    self._isPaused = isPaused
+  }
 	
 	var body: some View {
-		WaveformView(audioURL: fileURL, configuration: .init(
-      style: .striped(.init(color: .gray, width: 2, spacing: 1, lineCap: .butt)),
-      verticalScalingFactor: 1)) {
-			ProgressView()
-				.controlSize(.extraLarge)
-				.progressViewStyle(.linear)
-		}
-		
-		
+    Group {
+      if isPaused {
+        ProgressView().progressViewStyle(.circular).controlSize(.small)
+          .contentTransition(.opacity)
+      } else {
+        WaveformView(audioURL: fileURL, configuration: .init(
+          style: .striped(.init(color: .gray, width: 2, spacing: 1, lineCap: .butt)),
+          verticalScalingFactor: 1)) {
+            ProgressView()
+              .controlSize(.extraLarge)
+              .progressViewStyle(.linear)
+          }
+          .id(randomID) // destroy the old view manually
+      }
+    }
+    .animation(.easeInOut(duration: 0.2), value: isPaused)
 	}
 }
 

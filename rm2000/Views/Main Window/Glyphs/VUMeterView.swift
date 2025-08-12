75d51fff733245c49807b0db2f23d128bad84d6d
Marcelo Mendez <marceloexc@tutanota.de>
2025-08-12 17:43:57 -0500

    VUMeter: go back to zero when not recording

--- rm2000/Views/Main Window/Glyphs/VUMeterView.swift
+++ rm2000/Views/Main Window/Glyphs/VUMeterView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import Combine
 
 struct VUMeter: View {
 	
@@ -6,28 +7,29 @@ struct VUMeter: View {
 	
 	var body: some View {
 		GeometryReader
-		{ geometry in
-			ZStack(alignment: .bottom){
-				
-				// Colored rectangle in back of ZStack
-				Rectangle()
-					.fill(Color("LCDTextColor"))
-					.frame(height: geometry.size.height * CGFloat(self.volumeAsString))
-					.animation(.easeOut(duration:0.05))
+    { geometry in
+      ZStack(alignment: .bottom){
+        
+        // Colored rectangle in back of ZStack
+        Rectangle()
+          .fill(Color("LCDTextColor"))
+          .frame(height: geometry.size.height * CGFloat(self.volumeAsString))
+          .animation(.easeOut(duration:0.05))
           .blur(radius: 1.85)
-				
-				// idle blocks for volume
-				Rectangle()
-				.fill(Color.black.opacity(0.2))			}
-			.padding(geometry.size.width * 0.2)
-			.onReceive(NotificationCenter.default.publisher(for: .audioLevelUpdated)) { levels in
-        if let level = levels.userInfo?["level"] as? Float {
-					volumeAsString = level
-				} else {
-					volumeAsString = 0.0
-				}
-			}
-		}
+        
+        // idle blocks for volume
+        Rectangle()
+        .fill(Color.black.opacity(0.2))			}
+      .padding(geometry.size.width * 0.2)
+      .onReceive(
+        NotificationCenter.default.publisher(for: .audioLevelUpdated)
+          .map { $0.userInfo?["level"] as? Float ?? 0.0 }
+          .merge(with: Just(0.0).delay(for: .milliseconds(500), scheduler: RunLoop.main))
+          .removeDuplicates()
+      ) { level in
+        volumeAsString = level
+      }
+    }
 	}
 }
 

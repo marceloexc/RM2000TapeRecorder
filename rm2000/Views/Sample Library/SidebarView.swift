5302a3b8b1613bd3bc9ad70c833e29b249491d64
Marcelo Mendez <marceloexc@tutanota.de>
2025-10-29 12:56:59 -0500

    ui: change untagged palette to black on tahoe

--- rm2000/Views/Sample Library/SidebarView.swift
+++ rm2000/Views/Sample Library/SidebarView.swift
@@ -1,46 +1,53 @@
 import SwiftUI
 
 enum SampleFilterPredicate: Hashable {
-	case all
-	case untagged
-	case tagged(String)
+  case all
+  case untagged
+  case tagged(String)
 }
 
 struct SidebarView: View {
-	@ObservedObject var viewModel: SampleLibraryViewModel
-	
-	init(viewModel: SampleLibraryViewModel) {
-		self.viewModel = viewModel
-	}
-	
-	var body: some View {
-		List(selection: $viewModel.sidebarSelection) {
-			Section(header: Text("Collections")) {
-				NavigationLink(value: SampleFilterPredicate.all) {
-					Label("All Recordings", systemImage: "folder")
-				}
-				NavigationLink(value: SampleFilterPredicate.untagged) {
-					HStack {
-						Image("untagged")
-							.symbolRenderingMode(.palette)
-							.foregroundStyle(.red, Color.accentColor)
-						
-						Text("Untagged")
-					}
-				}
-			}
-			Section(header: Text("Available tags")) {
-				ForEach(viewModel.indexedTags, id: \.self) { tagName in
-					NavigationLink(value: SampleFilterPredicate.tagged(tagName)) {
-						Label("\(tagName)", systemImage: "number")
-					}
-				}
-			}
-		}
-	}
+  @ObservedObject var viewModel: SampleLibraryViewModel
+
+  init(viewModel: SampleLibraryViewModel) {
+    self.viewModel = viewModel
+  }
+
+  var body: some View {
+    List(selection: $viewModel.sidebarSelection) {
+      Section(header: Text("Collections")) {
+        NavigationLink(value: SampleFilterPredicate.all) {
+          Label("All Recordings", systemImage: "folder")
+        }
+        NavigationLink(value: SampleFilterPredicate.untagged) {
+          HStack {
+            Image("untagged")
+              .symbolRenderingMode(.palette)
+
+              .modifier { content in
+                if #available(macOS 26.0, *) {
+                  content.foregroundStyle(.red, Color.primary)
+                } else {
+                  content.foregroundStyle(.red, Color.accentColor)
+                }
+              }
+
+            Text("Untagged")
+          }
+        }
+      }
+      Section(header: Text("Available tags")) {
+        ForEach(viewModel.indexedTags, id: \.self) { tagName in
+          NavigationLink(value: SampleFilterPredicate.tagged(tagName)) {
+            Label("\(tagName)", systemImage: "number")
+          }
+        }
+      }
+    }
+  }
 }
 
 #Preview("Sidebar View") {
-	let vm = SampleLibraryViewModel()
-	return SidebarView(viewModel: vm)
+  let vm = SampleLibraryViewModel()
+  return SidebarView(viewModel: vm)
 }
